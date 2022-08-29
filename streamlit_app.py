fab64a1e2fd079225a5f8165ce3925885d5dd397
Katri Tuunanen <40175786+katrii@users.noreply.github.com>
2022-08-29 13:15:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title("My Parents' New Healthy Diner")
-streamlit.header('Breakfast Menu')
-streamlit.text('Instagrammable Omega 3 & Blueberry Oatmeal')
-streamlit.text('Very Trendy Avocado Toast')
-streamlit.text('Free-Range Eggs with Fancy Toppings')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Instagrammable Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Very Trendy Avocado Toast')
+streamlit.text('🐔 Free-Range Eggs with Fancy Toppings')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
